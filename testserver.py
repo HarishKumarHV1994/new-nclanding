fb4541ec82308920c021281eef59d75a7b0a4e95
HARISH KUMAR H V <harishkumarhv1994@gmail.com>
2021-02-19 11:41:50 +0530

    updated

--- testserver.py
+++ testserver.py
@@ -19,59 +19,59 @@ app = Bottle(__name__)
 #db = client.cas
 
 
-@route('/hello/<name>')
+@app.route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-@route('/ncdlanding')
+@app.route('/')
 def root():
 	# return template('templates/login.tpl', msg='')
 	return static_file('ncdlanding.html', root='templates/')
 
-@route('/ncdhome')
+@app.route('/ncdhome')
 def ncd_home():
 	#data = get_stress_json()
     # to be created
 	return static_file('ncd_home.html', root='templates/')
 
-@route('/ncdStress')
+@app.route('/ncdStress')
 def ncd_stress():
 	data = get_stress_json()
 	return template('templates/assessment_stress_home.tpl', data=data)
 
-@route('/ncdScreening')
+@app.route('/ncdScreening')
 def ncd_screening():
 	data = get_screening_json()
 	return template('templates/assessment_screening_home.tpl', data=data)
 
-@route('/ncdRapid')
+@app.route('/ncdRapid')
 def ncd_rapid():
 	data = get_rapid_json()
 	return template('templates/assessment_rapid_home.tpl', data=data)
 
-@route('/ncdFeasibility')
+@app.route('/ncdFeasibility')
 def ncd_feasibility():
 	data = get_feasibility_json()
 	return template('templates/assessment_feasibility_home.tpl', data=data)
 
 
-@route('/ncdscreening_score')
+@app.route('/ncdscreening_score')
 def ncdStress_score():
 	return static_file('assessment_screening_score.html', root='templates/')
 
-@route('/ncdfeasibility_score')
+@app.route('/ncdfeasibility_score')
 def ncdFeasibility_score():
 	return static_file('assessment_feasibility_score.html', root='templates/')
 
-@route('/ncdrapid_score')
+@app.route('/ncdrapid_score')
 def ncdRapid_score():
 	return static_file('assessment_rapid_score.html', root='templates/')
 
-@route('/ncdStress_score')
+@app.route('/ncdStress_score')
 def assessment_score():
 	return static_file('assessment_stress_score.html', root='templates/')
 
-@post('/add_ncdstress_assessment')
+@app.post('/add_ncdstress_assessment')
 def add_ncd_Stress():
 
 	assessment_data = request.forms.get('data')
@@ -85,7 +85,7 @@ def add_ncd_Stress():
 	#cur = db.stress_assessments.insert({'ncd_stress_data': assessment_data, 'time_stamp': time_stamp})
 	return {'status': 'ok'}
 
-@post('/add_ncdfeasibility_assessment')
+@app.post('/add_ncdfeasibility_assessment')
 def add_ncd_feasibility():
 
 	assessment_data = request.forms.get('data')
@@ -99,7 +99,7 @@ def add_ncd_feasibility():
 	#cur = db.stress_assessments.insert({'ncd_stress_data': assessment_data, 'time_stamp': time_stamp})
 	return {'status': 'ok'}
 
-@post('/add_ncdrapid_assessment')
+@app.ost('/add_ncdrapid_assessment')
 def add_ncd_rapid():
 
 	assessment_data = request.forms.get('data')
@@ -113,7 +113,7 @@ def add_ncd_rapid():
 	#cur = db.stress_assessments.insert({'ncd_rapid_data': assessment_data, 'time_stamp': time_stamp})
 	return {'status': 'ok'}
 
-@post('/add_ncdscreening_assessment')
+@app.post('/add_ncdscreening_assessment')
 def add_ncd_screening():
 
 	assessment_data = request.forms.get('data')
@@ -128,14 +128,14 @@ def add_ncd_screening():
 	return {'status': 'ok'}
 
 
-@route('/<filename:re:.*\.*>')
+@app.route('/<filename:re:.*\.*>')
 def javascripts(filename):
     return static_file(filename, root='static')
 
-@hook('after_request')
+@app.hook('after_request')
 def enable_cors():
 	response.headers['Access-Control-Allow-Origin'] = '*'
 	response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
 	response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
-run(host='localhost', port=8080)
+#run(host='localhost', port=8080)
